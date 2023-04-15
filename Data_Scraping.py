b3f1c14dfff74417a05be6365253ef4c13ea16d7
dannymirxa <96708252+dannymirxa@users.noreply.github.com>
2023-04-15 12:15:46 +0800

    added Data_Scraping.py
    Scrape first 50 pages and load the result into Polars dataframe before producing new_result.csv

---
+++ Data_Scraping.py
@@ -0,0 +1,66 @@
+import sys
+sys.setrecursionlimit(100000)
+
+import httpx
+from dataclasses import dataclass
+from bs4 import BeautifulSoup as bs
+import polars as pl
+
+@dataclass
+class Property:
+    House : str
+    Price : str
+    Location : str
+    Category : str
+    Size : str
+    Bedroom : str
+    Bathroom : str    
+    
+def get_html(page):
+    url = f"https://www.mudah.my/kuala-lumpur/properties-for-rent?o={page}"
+    resp = httpx.get(url)
+    return bs(resp.text, 'html.parser')
+
+def parse_properties(html):
+    house_catalogues = html.find_all("div", class_ = "sc-ktHwxA iXsADi")
+    results = []
+    for catalogue in house_catalogues:
+        bedroom = None
+        bathroom = None
+        try:
+            bedroom = catalogue.find("div", {"title" : "Bedrooms"}).div.string
+        except AttributeError:
+            pass
+        try:
+            bedroom = catalogue.find("div", {"title" : "Bathrooms"}).div.string
+        except AttributeError:
+            pass
+
+        properties = Property(
+            House = catalogue.find("a")["title"],
+            Location = catalogue.find("span", class_="sc-uJMKN fgsfIN").string,
+            Price = catalogue.find("div", class_="sc-jtRfpW eAwSpO").string,
+            Category = catalogue.find("div", class_="sc-dfVpRl eigVMP").string,
+            Size = catalogue.find("div", {"title" : "Size"}).div.string,
+            Bedroom = bedroom,
+            Bathroom = bathroom
+            )
+        results.append(properties)
+    return results
+
+def create_result():
+    res_result = []
+    for page_number in range (1,50):
+        html = get_html(page_number)
+        #print(html.css_first("title").text())
+        res = parse_properties(html)
+        res_result.extend(res)
+    #return res_result
+    df = pl.DataFrame(res_result)
+    df.write_csv('/mnt/d/Master Data Science/Principles of Data Science/new_result.csv')
+    
+def main():
+    create_result()
+    
+if __name__ == "__main__":
+    main()
